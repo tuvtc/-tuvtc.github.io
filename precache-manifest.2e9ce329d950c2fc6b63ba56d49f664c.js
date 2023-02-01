a56ced7e26e54469a69194d1190ab74022d7e4ef
tuvtcfx10680 <tuvtcfx10680@funix.edu.vn>
2023-02-01 18:29:33 +0700

    Updates

---
+++ precache-manifest.2e9ce329d950c2fc6b63ba56d49f664c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ec2ef6006e6c3b475307543560f2d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17d1baeabf2ce87fa32c",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "96fd4d56e06f849f3fa3",
+    "url": "/static/css/main.ede003c0.chunk.css"
+  },
+  {
+    "revision": "17d1baeabf2ce87fa32c",
+    "url": "/static/js/2.20319d01.chunk.js"
+  },
+  {
+    "revision": "02a3f81fc9b5d4efe0c8e2e0cd098576",
+    "url": "/static/js/2.20319d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fd4d56e06f849f3fa3",
+    "url": "/static/js/main.98a646bf.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.98a646bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260705052eb5a5c8bd41",
+    "url": "/static/js/runtime-main.d3cf5db1.js"
+  },
+  {
+    "revision": "dc2beb32991845519aee38f19d9cfd1c",
+    "url": "/static/media/search.dc2beb32.svg"
+  }
+]);
